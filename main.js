4dbaa8b8654e126ecaf35a742353c385e9297a1d
leangjia <liangjia@qq.com>
2020-05-05 23:03:21 +0800

    更换源码

---
+++ main.js
@@ -0,0 +1,49 @@
+var img = document.getElementById("showimg");
+
+function random(){
+//      var number = parseInt(1000*Math.random())%5;
+//      if (number == 0)
+//      {
+        img.src = "./img/wechat1.png";
+//      }
+//      if (number == 1)
+//      {
+//          img.src = "./img/wechat2.png";
+//      }
+//      if (number == 2)
+//      {
+//          img.src = "./img/wechat3.png";
+//      }
+//      if (number == 3){
+//          img.src = "./img/wechat4.png";
+//      }
+//      if (number == 4){
+//          img.src = "./img/wechat5.png";
+//      }
+     // if (number == 5){
+     //     img.src = "./img/wechat5.png";
+     // }
+
+//      if (number == 6){
+//          img.src = "./img/wechat7.png";
+//      }
+//      if (number == 7){
+//          img.src = "./img/wechat8.png";
+//      }
+//      if (number == 8){
+//          img.src = "./img/wechat9.png";
+//      }
+//      if (number == 9){
+//          img.src = "./img/wechat10.png";
+//      }
+//      console.log(number);
+
+
+}
+
+
+function showme(){
+    random();
+}
+
+showme();
